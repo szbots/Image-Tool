cc1b3f5ad737f6ddf9bb1d00743ec99a4fdaf629
szsupunma <86388269+szsupunma@users.noreply.github.com>
2022-02-24 23:05:43 -0800

    Got update !

--- szbot/helpers/fsub.py
+++ szbot/helpers/fsub.py
@@ -1,52 +1,23 @@
-import os
-import asyncio
-from config import UPDATES_CHANNEL
-from pyrogram import Client
-from pyrogram.errors import FloodWait, UserNotParticipant
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from functools import wraps
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
-async def ForceSub(bot: Client, event: Message):
-    try:
-        invite_link = await bot.create_chat_invite_link(chat_id=(int(UPDATES_CHANNEL) if UPDATES_CHANNEL.startswith("-100") else UPDATES_CHANNEL))
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        fix_ = await ForceSub(bot, event)
-        return fix_
-    except Exception as err:
-        print(f"Unable to do Force Subscribe to {UPDATES_CHANNEL}\n\nError: {err}\n\nContact Support Group: https://t.me/slbotzone")
-        return 200
-    try:
-        user = await bot.get_chat_member(chat_id=(int(UPDATES_CHANNEL) if UPDATES_CHANNEL.startswith("-100") else UPDATES_CHANNEL), user_id=event.from_user.id)
-        if user.status == "kicked":
-            await bot.send_message(
-                chat_id=event.from_user.id,
-                text="Sorry Dear, You are Banned to use me ☹️\nFeel free to say in our [Support Group](https://t.me/slbotzone).",
-                parse_mode="markdown",
-                disable_web_page_preview=True,
-                reply_to_message_id=event.message_id
-            )
-            return 400
-        else:
-            return 200
-    except UserNotParticipant:
-        await bot.send_message(
-            chat_id=event.chat.id,
-            text="<b>Hello {} 👋</b>\n\n<b>You cant use me untill subscribe our updates channel ☹️</b>\n<b>So Please join our updates channel by the following button and restart our bot by ( /start ) command 😊</b>".format(event.from_user.mention),
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("Join Our Updates Channel 🔔", url=invite_link.invite_link)
-                    ]
-                ]
-            ),
-            disable_web_page_preview=True,
-            reply_to_message_id=event.message_id
-        )
-        return 400
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        fix_ = await ForceSub(bot, event)
-        return fix_
-    except Exception as err:
-        print(f"Something Went Wrong! Unable to do Force Subscribe.\nError: {err}\n\nContact Support Group: https://t.me/slbotzone")
-        return 200
+
+CAPTION_BTN = InlineKeyboardMarkup(
+            [[InlineKeyboardButton("News  Channel", url="https://t.me/szteambots")]])
+
+def ForceSub(func):
+    @wraps(func)
+    async def sz_message(_, message):
+        try:
+            await message._client.get_chat_member(-1001325914694, message.from_user.id)
+        except UserNotParticipant:
+            return await message.reply_text(
+            text="""
+**🚫 Access Denied**
+ You Must Join [My News Channel](https://t.me/szteambots)To Use Me. So, Please Join it & Try Again.
+            """,
+            reply_markup=CAPTION_BTN,
+            disable_web_page_preview=True) 
+        return await func(_, message)    
+    return sz_message
